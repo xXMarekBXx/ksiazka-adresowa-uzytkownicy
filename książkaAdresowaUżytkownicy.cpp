3734681e2243244b945268b7ef8df2b6d6d3ed93
xXMarekBXx <57196180+xXMarekBXx@users.noreply.github.com>
2021-01-29 11:43:10 +0100

    Add new feature from "Rejestracja użytkownikow"
    Od tej pory rejestracja użytkowników działa na plikach tekstowych

--- książkaAdresowaUżytkownicy.cpp
+++ książkaAdresowaUżytkownicy.cpp
@@ -12,12 +12,17 @@ using namespace std;
 
 struct Uzytkownik;
 
+vector<Uzytkownik> odczytUzytkownikowZPliku(vector <Uzytkownik> uzytkownicy);
+
 vector <Uzytkownik> rejestracja(vector <Uzytkownik> uzytkownicy);
 	
 int logowanie(vector <Uzytkownik> uzytkownicy);
 
+vector<Uzytkownik> usuwanieUzytkownika(vector <Uzytkownik> uzytkownicy, int idZalogowanegoUzytkownika);
+
 vector <Uzytkownik> zmianaHasla(vector <Uzytkownik> uzytkownicy, int idZalogowanegoUzytkownika);
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct Znajomy;
 
@@ -53,12 +58,11 @@ void balladaNaDowidzenia();
 int main()
 {
 	vector <Uzytkownik> uzytkownicy;
+	uzytkownicy = odczytUzytkownikowZPliku(uzytkownicy);
 
-	int idZalogowanegoUzytkownika = 0;
-	int iloscUzytkownikow = 0;
+	int idZalogowanegoUzytkownika = 0;	
 
 	vector<Znajomy> wektorZnajomych;
-
 	wektorZnajomych = odczytZnajomychZPliku(wektorZnajomych);
 
 
@@ -139,11 +143,69 @@ struct Uzytkownik
 	string login, haslo;
 };
 
+vector<Uzytkownik> odczytUzytkownikowZPliku(vector <Uzytkownik> uzytkownicy) {
+
+	cout << "Wczytano dane z pliku" << endl;
+
+	string linia;
+	int numerLinii = 1;
+	fstream plik;
+	Uzytkownik uzytkownik;
+
+	plik.open("uzytkownicy.txt", ios::in);
+
+
+	if (plik.is_open()) {
+		while (getline(plik, linia)) {
+			string Uzytkownik[3];
+			int dlugoscLinii = linia.length();
+			int beginOfWord = 0;
+			int lengthOfWord = 0;
+			int numerUzytkownika = 0;
+
+			for (int i = 0; i < dlugoscLinii; i++) {
+
+				if ((int)linia[i] == 124) {
+					lengthOfWord = i - beginOfWord;
+					Uzytkownik[numerUzytkownika] = linia.substr(beginOfWord, lengthOfWord);
+					beginOfWord = i + 1;
+					numerUzytkownika++;
+				}
+			}
+			uzytkownik.idUzytkownika = atoi(Uzytkownik[0].c_str());
+			uzytkownik.login = Uzytkownik[1];
+			uzytkownik.haslo = Uzytkownik[2];
+
+			uzytkownicy.push_back(uzytkownik);
+		}
+		plik.close();
+
+		return uzytkownicy;
+	}
+	else {
+		plik.close();
+		return uzytkownicy;
+	}
+
+	system("pause");
+}
+
 vector <Uzytkownik> rejestracja(vector <Uzytkownik> uzytkownicy)
 {
 	system("cls");
+	fstream plik;
 	string loginNowegoUzytkownika, haslo;
 	Uzytkownik nowyUzytkownik;
+
+	plik.open("uzytkownicy.txt");
+	if (plik.good()) {
+		plik.close();
+	}
+	else {
+		plik.open("uzytkownicy.txt", ios::out);
+		plik.close();
+	}
+
 	cout << "Podaj nazwe uzytkownika: ";
 	cin >> loginNowegoUzytkownika;
 
@@ -179,12 +241,22 @@ vector <Uzytkownik> rejestracja(vector <Uzytkownik> uzytkownicy)
 	nowyUzytkownik = { ostatnieID + 1, loginNowegoUzytkownika, haslo };
 
 	uzytkownicy.push_back(nowyUzytkownik);
-	cout << endl;
-	cout << "Uzytkownik dodany" << endl;
-	cout << endl;
-	system("Pause");
 
-	return uzytkownicy;
+	plik.open("uzytkownicy.txt", ios::out | ios::app);
+	if (plik.good()) {
+		plik << uzytkownicy[iloscUzytkownikow].idUzytkownika << "|";
+		plik << uzytkownicy[iloscUzytkownikow].login << "|";
+		plik << uzytkownicy[iloscUzytkownikow].haslo << "|" << endl;
+		cout << endl;
+		cout << "Uzytkownik dodany" << endl;
+		cout << endl;
+		system("Pause");
+		return uzytkownicy;
+	}
+	else {
+		cout << "Nie udalo sie otworzyc pliku" << endl;
+		system("Pause");
+	}
 }
 
 int logowanie(vector <Uzytkownik> uzytkownicy)
@@ -252,8 +324,53 @@ int logowanie(vector <Uzytkownik> uzytkownicy)
 	return 0;
 }
 
+vector<Uzytkownik> usuwanieUzytkownika(vector <Uzytkownik> uzytkownicy, int idZalogowanegoUzytkownika) {
+
+	string wyborUrzytkownika;
+	fstream plik;
+
+	plik.open("temp.txt", ios::out);
+	if (plik.good()) {
+
+		for (int i = 0; i < uzytkownicy.size(); i++) {
+			if (idZalogowanegoUzytkownika == uzytkownicy[i].idUzytkownika) {
+				continue;
+			}
+			else {
+				plik << uzytkownicy[i].idUzytkownika << "|";
+				plik << uzytkownicy[i].login << "|";
+				plik << uzytkownicy[i].haslo << "|" << endl;
+			}
+		}
+
+		plik.close();
+
+		remove("uzytkownicy.txt");
+		rename("temp.txt", "uzytkownicy.txt");
+
+		int numerZnajomegoDoUsuniecia = 0;
+		for (int i = 0; i < uzytkownicy.size(); i++) {
+			if (uzytkownicy[i].idUzytkownika == idZalogowanegoUzytkownika) {
+				numerZnajomegoDoUsuniecia = i;
+			}
+		}
+		uzytkownicy.erase(uzytkownicy.begin() + numerZnajomegoDoUsuniecia);
+
+		return uzytkownicy;
+	}
+	else {
+		plik.close();
+		cout << "Blad odczytu pliku!" << endl;
+		cout << endl;
+		system("Pause");
+		return uzytkownicy;
+	}
+}
+
 vector <Uzytkownik> zmianaHasla(vector <Uzytkownik> uzytkownicy, int idZalogowanegoUzytkownika)
 {
+	Uzytkownik uzytkownikZNowymHaslem;
+
 	int indexDoEdycji;
 	string noweHaslo;
 	cout << endl;
@@ -266,12 +383,36 @@ vector <Uzytkownik> zmianaHasla(vector <Uzytkownik> uzytkownicy, int idZalogowan
 		}
 	}
 	uzytkownicy[indexDoEdycji].haslo = noweHaslo;
+
+	usuwanieUzytkownika(uzytkownicy, idZalogowanegoUzytkownika);
+
+	fstream plik;
+
+	plik.open("uzytkownicy.txt", ios::out | ios::app);
+	if (plik.good()) {
+		plik << uzytkownicy[indexDoEdycji].idUzytkownika << "|";
+		plik << uzytkownicy[indexDoEdycji].login << "|";
+		plik << uzytkownicy[indexDoEdycji].haslo << "|" << endl;
+	}
+	else {
+		cout << "Nie udalo sie otworzyc pliku" << endl;
+		system("Pause");
+	}
+
+	uzytkownicy.push_back(uzytkownikZNowymHaslem);
+
 	cout << endl;
 	cout << "Udalo sie zmienic haslo." << endl;
 	cout << endl;
 	system("Pause");
+
 	return uzytkownicy;
 }
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 struct Znajomy {
 	int id;
